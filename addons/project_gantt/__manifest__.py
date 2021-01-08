9ac768e88d37b0dde614e8749abdf4f135dfa7ba
rohit22799 <rohit@techreceptives.com>
2021-01-08 17:11:10 +0530

    [ADD] Web Gantt Chart

---
+++ addons/project_gantt/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Project Gantt Chart Demo',
+    'category': 'Education',
+    'version': '14.0',
+    'summary': 'Project Gantt Chart Demo',
+    'depends': [
+        'web_gantt',
+        'project'
+    ],
+    'data': [
+        'views/project_task_inherit.xml',
+    ],
+    'demo': ['data/project_task_demo.xml'],
+    'qweb': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ addons/web_gantt/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Web Gantt Chart',
+    'category': 'Education',
+    'version': '14.0',
+    'summary': 'Gantt Chart',
+    'depends': [
+        'web'
+    ],
+    'data': [
+        'views/assets.xml',
+    ],
+    'qweb': [
+        'static/src/xml/*.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
