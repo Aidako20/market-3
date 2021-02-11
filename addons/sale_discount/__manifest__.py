2271c4b2c232780d3739795cde0b5ab7c961b6da
Renzo Meister <rm@jamotion.ch>
2021-02-11 18:30:20 +0100

    [ADD] sale_discount, account_discount: adds feature to add global document discount on sale orders and invoices

--- addons/sale_discount/__manifest__.py
+++ addons/sale_discount/__manifest__.py
@@ -17,7 +17,7 @@
     'data': [
         # 'security/ir.model.access.csv',
         # 'views/res_config_settings_views.xml',
-        'views/product_views.xml',
+        # 'views/product_views.xml',
         'views/sale_views.xml',
         'reports/sale_report_templates.xml',
     ],
